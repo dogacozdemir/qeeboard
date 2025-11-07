21a3dc5d7b8d259c3751cb2343d8e0c7f78a7f83
Uygar Dogac Ozdemir <uygardogacozdemir@192.168.1.7>
2025-11-08 00:39:36 +0300

    Add basename to BrowserRouter for designer app

--- designer/src/App.tsx
+++ designer/src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/designer">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/svg-demo" element={<SVGDemo />} />
